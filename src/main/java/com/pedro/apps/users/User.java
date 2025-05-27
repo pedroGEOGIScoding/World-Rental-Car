ea809fd4841b60fbe5438b9d9d95460caab8dc15
pedroGEOGIScoding <pedromartinezduran@gmail.com>
2025-05-27 13:56:40 +0200

    Changes in the User.java and enum out of User bean and created outside. App opens and running fine and retrieving to cars

--- src/main/java/com/pedro/apps/users/User.java
+++ src/main/java/com/pedro/apps/users/User.java
@@ -1,12 +1,10 @@
 package com.pedro.apps.users;
 
-import com.pedro.apps.delegations.Car;
+
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
-
-import java.util.HashMap;
 
 @DynamoDbBean
 public class User {
@@ -16,13 +14,12 @@ public class User {
   private String email;
   private String fullName;
   private String phone;
-  public enum role {CUSTOMER, ADMIN};
-  private role userRole;
+  private UserRole userRole;
   
   // Default constructor required for DynamoDB's enhanced client
   public User() {
 	this.userId = "";
-	this.userRole = role.CUSTOMER; // Default status
+	this.userRole = UserRole.CUSTOMER; // Default status
   }
   
   public User(String userId, String operation, String username, String email, String fullName, String phone) {
@@ -89,11 +86,11 @@ public class User {
   }
   
   @DynamoDbAttribute("userRole")
-  public role getUserRole() {
+  public UserRole getUserRole() {
 	return userRole;
   }
   
-  public void setUserRole(role userRole) {
+  public void setUserRole(UserRole userRole) {
 	this.userRole = userRole;
   }
 }
